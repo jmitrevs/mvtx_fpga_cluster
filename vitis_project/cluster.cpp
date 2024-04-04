e82d095a2dcda3632ae523579468cccb6d536810
Cameron Dean <cameron.dean@cern.ch>
2024-04-04 17:20:04 -0400

    Streamed clusterized, no extra code commented out

--- vitis_project/cluster.cpp
+++ vitis_project/cluster.cpp
@@ -2,7 +2,6 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: X11
 */
-
 #include "cluster.h"
 
 #include <ap_fixed.h>
@@ -25,8 +24,6 @@ void writeCluster(ap_uint<5> index, cluster_t& myCluster) //Can we handle moving
   //Remember to weight from center of pixel, not the edge (add 0.5 to each pixel)
   //This should be right but the rows count from the top left of the chip (so row 0 is at +0.687cm while rown 511 is at -0.687cm)
 
-  //float precise_col2 = 0;
-  //float precise_row2 = 0;
   ap_ufixed<17,13> precise_col = 0; 
   ap_ufixed<16,12> precise_row = 0; 
 
@@ -39,30 +36,21 @@ void writeCluster(ap_uint<5> index, cluster_t& myCluster) //Can we handle moving
     ++nConstituents;
   }
 
-  writeCluster_getPreciseCentroid:
   ap_ufixed<2,0> offset = 0.5;
+  writeCluster_getPreciseCentroid:
   for (int i = 0; i < maxPixelsInCluster; i++)
   {
     //#pragma HLS unroll
-    #pragma HLS PIPELINE II=5
     if (i == nConstituents) break; 
-    //precise_col2 += (clusterConstituents[index][i].first + 0.5)/nConstituents;
-    //precise_row2 += (clusterConstituents[index][i].second+0.5)/nConstituents;
-    precise_col  += (clusterConstituents[index][i].first);//+offset)/ nConstituents;
-    precise_row  += (clusterConstituents[index][i].second);//+offset)/nConstituents;
+    precise_col  += (clusterConstituents[index][i].first);
+    precise_row  += (clusterConstituents[index][i].second);
   }
 
-//  ap_ufixed<1,0> offset = 0.5;
-//#ifndef __SYNTHESIS__
-//  std::cout << "offset is: " << offset << std::endl;
-//#endif
-//  //float offset = 0.5; 
-//
   precise_col += (offset*nConstituents); 
   precise_row += (offset*nConstituents); 
- //
- precise_col = precise_col/nConstituents; 
- precise_row = precise_row/nConstituents; 
+ 
+  precise_col = precise_col/nConstituents; 
+  precise_row = precise_row/nConstituents; 
 
   writeCluster_makeClusterInfo:
   myCluster.first.first = hls::floor(precise_col); //Centroid column
@@ -70,23 +58,15 @@ void writeCluster(ap_uint<5> index, cluster_t& myCluster) //Can we handle moving
   // better resource usage than conditional if statement 
   ap_ufixed<2,0> diffCol = precise_col - myCluster.first.first; 
   ap_ufixed<2,0> diffRow = precise_row - myCluster.first.second;
-//#ifndef __SYNTHESIS__
-//  std::cout << "precise col is " << precise_col << " precise_row is " << precise_row << std::endl;
-  //std::cout << "precise col2 is " << precise_col2 << " precise_row2 is " << precise_row2 << std::endl;
-
-//  std::cout << "diffCol is " << diffCol << " diffRow is " << diffRow << std::endl;
-//#endif
-  //float diffCol = precise_col - myCluster.first.first; 
-  //float diffRow = precise_row - myCluster.first.second;
 
   if(diffCol < offset){
     myCluster.second.first = 0; 
-    if(diffRow < offset)myCluster.second.second = 0; 
+    if (diffRow < offset) myCluster.second.second = 0; 
     else myCluster.second.second = 1; 
   }
   else{
     myCluster.second.first = 1; 
-    if(diffRow < offset)myCluster.second.second = 0; 
+    if (diffRow < offset) myCluster.second.second = 0; 
     else myCluster.second.second = 1;
   }
 
@@ -98,6 +78,7 @@ void writeCluster(ap_uint<5> index, cluster_t& myCluster) //Can we handle moving
 	  writeCluster_clearPixels:
     for (unsigned int k = 0; k < maxPixelsInCluster; k++)
     {
+      #pragma HLS PIPELINE II=5
       //#pragma HLS unroll
       //#ifndef __SYNTHESIS__
       //std::cout << "Cluster index  " << index << " has constituent (" << clusterConstituents[j][k].first << ", " << clusterConstituents[j][k].second << ")" << std::endl;
@@ -110,9 +91,8 @@ void writeCluster(ap_uint<5> index, cluster_t& myCluster) //Can we handle moving
 
 void cluster_algo(hls::stream<hit_t> &source, hls::stream<cluster_t> &sink)
 {
-  cluster_t constructedCluster; //Can we pass the whole sink to writeCluster?
+  cluster_t constructedCluster;
   ClusterAlgoBegin_Loop:
-  //for (unsigned int k = 0; k < source.size(); k++) 
   while (!source.empty())
   {
     //#pragma HLS loop_flatten off 
@@ -160,7 +140,6 @@ void cluster_algo(hls::stream<hit_t> &source, hls::stream<cluster_t> &sink)
           for (int n = 0; n < maxPixelsInCluster; n++)
           {
             //#pragma HLS unroll
-  
   	        hit_t pixelPair = clusterConstituents[i][n]; 
   	        if (pixelPair == pairInitilizer) break;
   	        ++nConstituents;
@@ -180,12 +159,13 @@ void cluster_algo(hls::stream<hit_t> &source, hls::stream<cluster_t> &sink)
   	
     }
   } // end the for loop
-
+//Can maybe get rid of the memory flush as we stream now
    while(clusterConstituents[0][0] != pairInitilizer)
    {
      writeCluster(0, constructedCluster);
      sink.write(constructedCluster);
    }
+
 }
 
 void read_data(hit_t input[nHits], hls::stream<hit_t> &buf)
@@ -196,17 +176,7 @@ void read_data(hit_t input[nHits], hls::stream<hit_t> &buf)
     buf << input[i];
   }
 }
-/*
-void write_data(hls::stream<cluster_t> &buf, cluster_t output[nClusters])
-{
-  WR_Loop_Row:
-  for (int i = 0; i < clusBufferSize; i++)
-  while (!buf.empty())
-  {
-    buf << output[i] = ;
-  }
-}
-*/
+
 void cluster(hit_t in[nHits], cluster_t out[nClusters])
 {
    hls::stream<hit_t> buf_in;
@@ -214,18 +184,10 @@ void cluster(hit_t in[nHits], cluster_t out[nClusters])
 
    // Read input data. Fill the internal buffer.
    read_data(in, buf_in);
-/*
-  #ifndef __SYNTHESIS__
-   hit_t streamed_hit = buf_in.read();
-   int col = streamed_hit.first;
-   int row = streamed_hit.second;
-   std::cout << "Streamed hit: (col, row) = (" << col << ", " << row << ")" << std::endl;
-  #endif
-*/
+
    cluster_algo(buf_in, buf_out);
 
    // Write out the results
-   //write_data(buf_out, out);
    int iVal = 0;
    while (!buf_out.empty())
    {
@@ -237,15 +199,5 @@ void cluster(hit_t in[nHits], cluster_t out[nClusters])
    for (unsigned int i = 0; i < nClusters; i++)
      std::cout << "Cluster info: (col, row) = (" << out[i].first.first << ", " << out[i].first.second << "), (col quad, row quad) = (" << out[i].second.first << ", " << out[i].second.second << ")" << std::endl;
    #endif
-/*
-  #ifndef __SYNTHESIS__
-   cluster_t streamed_clus = buf_out.read();
-   int col = streamed_clus.first.first;
-   int row = streamed_clus.first.second;
-   int quad_col = streamed_clus.second.first;
-   int quad_row = streamed_clus.second.second;
-   std::cout << "Streamed clus: (col, row) = (" << col << ", " << row << "), (col quad, row quad) = (" << quad_col << ", " << quad_row << ")" << std::endl;
-  #endif
-*/
 }
 
