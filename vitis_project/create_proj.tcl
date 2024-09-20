ee76c09cc57fd028034f83c06847342fc85cab4b
Jovan Mitrevski <jmitrevs@fnal.gov>
2024-09-20 13:59:37 -0500

    update the testbench

--- vitis_project/create_proj.tcl
+++ vitis_project/create_proj.tcl
@@ -3,6 +3,7 @@ open_project -reset "proj_clusterizer"
 set_top cluster_algo
 add_files cluster.cpp
 add_files -tb cluster_testbench.cpp
+add_files -tb in_jakub.dat
 
 open_solution -reset "solution"
 set_part {xcku115-flvf1924-2-e}
