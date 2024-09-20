189deaa12dc07251ca0e75d0a855628458662e38
Jovan Mitrevski <jmitrevs@fnal.gov>
2024-09-20 13:59:37 -0500

    update the testbench

--- vitis_project/cluster_testbench.cpp
+++ vitis_project/cluster_testbench.cpp
@@ -2,11 +2,15 @@
 #include <fstream>
 #include <utility>
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+void cluster(input_t in[nHits], output_t out[nClusters]);
 
 int main()
 {
   input_t inputHits[nHits];
-  cluster_t clusters[nClusters];
+  output_t clusters[nClusters];
  
   int retval = 0, i, j;
 
@@ -15,98 +19,45 @@ int main()
   // input of the clusterizer will be the output of the decoder
   // take sample file from jakub and use this
   // output with normal formal is in.dat
-  inFile = fopen("in_jakub_singleEvent.dat", "r");
+  std::ifstream fin("in_jakub.dat");
   
-  for (unsigned int i = 0; i < nHits; ++i)
-  {
-    // input bit that we will 
-    ap_int<20> inputBit; 
-    // declare the constituent bits 
-    unsigned int colBit; 
-    unsigned int rowBit;
-    unsigned int headerBit; 
-
-    fscanf(inFile, "%x %x/%x", &headerBit, &colBit, &rowBit);
-    inputBit[19] = headerBit; 
-    inputBit.range(18,9) = colBit; 
-    inputBit.range(8,0) = rowBit; 
-    inputHits[i] = inputBit; 
-  }
-
-  fclose(inFile);
-
-  for (unsigned int i = 0; i < 1; ++i)
-  {
-    std::cout << "Loop iteration " << i << std::endl;
-
-    for (unsigned int j = 0; j < nHits; j++)
-      std::cout << "Printing hit "  << j << ":   ("   << inputHits[j] << ")" << std::endl;
-
-    //send hit array to cluster top function
-    cluster(inputHits, clusters);
-  }
-
-
-  outFile=fopen("out.dat","w");
-
-  for (unsigned int i = 0; i < nClusters; i++){
-    fprintf(outFile, "%d %d %d %d\n", clusters[i].first.first, clusters[i].first.second, clusters[i].second.first, clusters[i].second.second);
-    std::cout  << clusters[i].first.first << " " << clusters[i].first.second << " " << clusters[i].second.first << " " << clusters[i].second.first << std::endl;
-  }
-  fclose(outFile);
-
-  // Compare the results file with the golden results
-  retval = system("diff --brief -w out.dat out.golden.dat");
-  if (retval != 0)
-  {
-    printf("Test failed  !!!\n");
-    retval=1;
-  }
-  else
-  {
-   printf("Test passed !\n");
-  }
-
-}
-
-void read_data(input_t input[nHits], hls::stream<input_t> &buf)
-{
- RD_Loop_Row:
-  for (int i = 0; i < hitBufferSize; i++)
-    {
-      buf << input[i];
-    }
-}
-
-void write_data(hls::stream<cluster_t> &buf, cluster_t output[nClusters]){
-  int iVal = 0;
-  while (!buf.empty())
-    {
-      output[iVal] = buf.read();
-      iVal++;
-    }
-}
-
-void cluster(input_t in[nHits], cluster_t out[nClusters])
-{
-
-#pragma HLS DATAFLOW
-
+  std::string iline;
+  
   hls::stream<input_t> buf_in;
-  hls::stream<cluster_t> buf_out;
+  hls::stream<output_t> buf_out;
 #pragma HLS STREAM variable=buf_in depth=1024 // needed for cosimulation to work
 #pragma HLS STREAM variable=buf_out depth=1024
 
-  // Read input data. Fill the internal buffer.
-  read_data(in, buf_in);
 
-  cluster_algo(buf_in, buf_out);
+  for (int iteration=0; std::getline(fin, iline); iteration++) {
+    std::replace(iline.begin(), iline.end(), '/', ' ');
+    std::stringstream ssin(iline);
 
-  write_data(buf_out, out);
+    unsigned int headerBit = 0; 
+    unsigned int colBit = 0; 
+    unsigned int rowBit = 0;
 
-#ifndef __SYNTHESIS__
-  for (unsigned int i = 0; i < nClusters; i++)
-    std::cout << "Cluster info: (col, row) = (" << out[i].first.first << ", " << out[i].first.second << "), (col quad, row quad) = (" << out[i].second.first << ", " << out[i].second.second << ")" << std::endl;
-#endif
+    ssin >> std::hex >> headerBit >> colBit >> rowBit;
+
+    //std::cout << std::hex << headerBit << " " << colBit << " " << rowBit << std::endl;
+
+    input_t inputBit;
+    inputBit[19] = headerBit; 
+    if (headerBit) {
+      inputBit(18, 0) = colBit;
+    } else {
+      inputBit(18,9) = colBit; 
+      inputBit(8,0) = rowBit; 
+    }
+
+    buf_in.write(inputBit);
+    cluster_algo(buf_in, buf_out);
+
+    output_t result;
+    if (buf_out.read_nb(result)) {
+      std::cout << "Iteration " << std::dec << iteration << " received " << std::hex << result << std::endl;
+    } else {
+      std::cout << "Iteration " << std::dec << iteration << " no ouput " << std::endl;
+    } 
+  }
 }
-
